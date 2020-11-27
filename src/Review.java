afcb0894ee29a8a8d04f2986abf471d0ccd48bc5
Freekode1ko <Freekode1ko@gmail.com>
2020-11-27 22:36:38 +0300

    POJO for comments

---
+++ src/Review.java
@@ -0,0 +1,25 @@
+
+public class Review 
+{
+	private String id;
+	private String restaurant_id;
+	private String name;
+	private String review;
+	private String rating;
+
+	public String getId() 
+	{return id;}
+	
+	public String getRestaurant_id() 
+	{return restaurant_id;}
+	
+	public String getName() 
+	{return name;}
+	
+	public String getReview() 
+	{return review;}
+	
+	public String getRating() 
+	{return rating;}
+
+}
