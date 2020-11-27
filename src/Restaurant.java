772bf38da2bfc5bb629f4c9cfd925651fead1b0f
Freekode1ko <Freekode1ko@gmail.com>
2020-11-27 22:36:23 +0300

    POJO for restorans

---
+++ src/Restaurant.java
@@ -0,0 +1,31 @@
+public class Restaurant 
+{
+	private String id;
+	private String name;
+	private String location;
+	private String price_range;
+	private String restaurant_id;
+	private String count;
+	private String average_rating;
+	
+	public String getId()
+	{return id;}
+	
+	public String getName()
+	{return name;}
+	
+	public String getLocation()
+	{return location;}
+	
+	public String getPrice_range()
+	{return price_range;}
+	
+	public String getRestaurant_id()
+	{return restaurant_id;}
+	
+	public String getCount()
+	{return count;}
+	
+	public String getAverage_rating()
+	{return average_rating;}
+}
