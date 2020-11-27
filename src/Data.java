dbf95e5582d3f145f1748d11bea09ef7defcf122
Freekode1ko <Freekode1ko@gmail.com>
2020-11-27 22:35:04 +0300

    Data POJO classes
    Data - for read all restor
    Data - for read all coment for selected rest

---
+++ src/Data.java
@@ -0,0 +1,9 @@
+import java.util.List;
+public class Data
+{
+	private List<Restaurant> restaurants = null;
+	
+	public List<Restaurant> getRestaurants() 
+	{return restaurants;}
+
+}
---
+++ src/Data2.java
@@ -0,0 +1,12 @@
+import java.util.List;
+public class Data2
+{
+	private Restaurant restaurants ;
+	private List<Review> reviews = null;
+	
+	public Restaurant getRestaurants() 
+	{return restaurants;}
+	
+	public List<Review> getReviews() 
+	{return reviews;}
+}
