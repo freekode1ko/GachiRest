6a2ce25ec9386b4a90b1711ecebbe822e08e03a9
Freekode1ko <Freekode1ko@gmail.com>
2020-11-27 22:35:58 +0300

    Main POJO for full json
    GETrest - for all rest
    GETrest - for all coments in selected rest

---
+++ src/GETrest.java
@@ -0,0 +1,16 @@
+public class GETrest 
+{
+	private String status;
+	private Integer results;
+	private Data data;
+
+	public String getStatus() 
+	{return status;	}
+
+	public Integer getResults() 
+	{return results;}
+
+	public Data getData() 
+	{return data;}
+	
+}
---
+++ src/GETrest2.java
@@ -0,0 +1,16 @@
+public class GETrest2 
+{
+	private String status;
+	private Integer results;
+	private Data2 data;
+
+	public String getStatus() 
+	{return status;	}
+
+	public Integer getResults() 
+	{return results;}
+
+	public Data2 getData() 
+	{return data;}
+	
+}
