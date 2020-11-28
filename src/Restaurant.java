182b44847cd8839293f5cc32da2023ea07404a38
Freekode1ko <Freekode1ko@gmail.com>
2020-11-28 19:42:58 +0300

    add new methods for creating new rest

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -3,7 +3,7 @@ public class Restaurant
 	private String id;
 	private String name;
 	private String location;
-	private String price_range;
+	private Integer price_range;
 	private String restaurant_id;
 	private String count;
 	private String average_rating;
@@ -14,11 +14,20 @@ public class Restaurant
 	public String getName()
 	{return name;}
 	
+	public void setName(String name)
+	{this.name = name;}
+	
 	public String getLocation()
 	{return location;}
 	
-	public String getPrice_range()
+	public void setLocation(String location)
+	{this.location = location;}
+	
+	public Integer getPrice_range()
 	{return price_range;}
+	
+	public void setPrice_range(Integer price_range)
+	{this.price_range = price_range;}
 	
 	public String getRestaurant_id()
 	{return restaurant_id;}
