1eb5ab11b4c9d75f365a19fdd5cab30269ae2f1b
Freekode1ko <Freekode1ko@gmail.com>
2020-11-28 19:42:35 +0300

    add settesr for adding new reviews

--- src/Review.java
+++ src/Review.java
@@ -2,24 +2,36 @@
 public class Review 
 {
 	private String id;
-	private String restaurant_id;
+	private Integer restaurant_id;
 	private String name;
 	private String review;
-	private String rating;
+	private Integer rating;
 
 	public String getId() 
 	{return id;}
 	
-	public String getRestaurant_id() 
+	public Integer getRestaurant_id() 
 	{return restaurant_id;}
+	
+	public void setRestaurant_id(Integer restaurant_id) 
+	{this.restaurant_id = restaurant_id;}
 	
 	public String getName() 
 	{return name;}
 	
+	public void setName(String name) 
+	{this.name = name;}
+	
 	public String getReview() 
 	{return review;}
 	
-	public String getRating() 
+	public void setReview(String review) 
+	{this.review = review;}
+	
+	public Integer getRating() 
 	{return rating;}
+	
+	public void setRating(Integer rating) 
+	{this.rating = rating;}
 
 }
